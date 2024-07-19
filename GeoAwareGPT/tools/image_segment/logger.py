2af7236c1b84b2ee8792911e47c6c05a6bb4824c
GeorgeRyan05 <122216046+GeorgeRyan05@users.noreply.github.com>
2024-07-19 18:33:12 +0530

    Set debug mode in my tool to false by default, otherwise it might pickle some stuff unnecessarily

--- GeoAwareGPT/tools/image_segment/logger.py
+++ GeoAwareGPT/tools/image_segment/logger.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from typing import Callable, Optional, Any
 import pickle
 
-DEBUG = True # Whether to log or not
+DEBUG = False # Whether to log or not
 LOG_DIR =  Path.joinpath(Path(Path(__file__).parent), 'log/')
 
 class Logger:
